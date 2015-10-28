3cc802f4fb6bdf5de059bbacfa5aca1b1da7c4a1
Ron Sanzone <ronald.lee.sanzone@gmail.com>
2015-10-28 00:00:42 -0400

    refactoring and cleaning up code

--- optimization_testrunner.py
+++ optimization_testrunner.py
@@ -1,25 +1,27 @@
-import FlightPathOptimization
-import GeneticFlightPath
+from FlightPathOptimization import FlightPathOptimization
+from GeneticFlightPath import GeneticFlightPath
+
 
 def main():
-  people = [('Seymour','BOS'),
-            ('Franny','RIC'),
-            ('Zooey','CAK'),
-            ('Walt','MIA'),
-            ('Buddy','ORD'),
-            ('Les','TYS')]
+    people = [('Seymour', 'BOS'),
+              ('Franny', 'RIC'),
+              ('Zooey', 'CAK'),
+              ('Walt', 'MIA'),
+              ('Buddy', 'ORD'),
+              ('Les', 'TYS')]
 
-  #Destination New York
-  destination = 'LGA'
+    # Destination New York
+    destination = 'LGA'
 
-  domain = [(0,8)]*(len(people)*2)
+    domain = [(0, 8)] * (len(people) * 2)
 
-  flightPathOptimization  = FlightPathOptimization(people, destination, 'realflight.txt')
-  geneticFlightPath = GeneticFlightPath(domain, flightPathOptimization.schedulecost)
-  s = geneticFlightPath.geneticoptimize()
+    flight_path_optimization = FlightPathOptimization(people, destination, 'realflight.txt')
+    genetic_flight_path = GeneticFlightPath(domain, flight_path_optimization.schedule_cost)
+    s = genetic_flight_path.genetic_optimize()
 
-  print "\n\nBest Schedule Found Through Optimization"
-  flightPathOptimization.printschedule(s)
+    print "\n\nBest Schedule Found Through Optimization"
+    flight_path_optimization.print_schedule(s)
+
 
 if __name__ == '__main__':
-  main()
+    main()
